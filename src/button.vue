68c83d0155f7c9a36c3277463b9426ebc1f43521
sunqi <sunqi@xiao100.com>
2021-03-02 15:38:48 +0800

    为 button 添加 icon 和 iconPosition 属性

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,27 @@
 <template>
-    <button class="qdd-button">按钮</button>
+    <button class="qdd-button" :class="{[`icon-${iconPosition}`]: true}">
+        <svg v-if="icon" class="icon" aria-hidden="true">
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script>
     export default {
-
+        // props: ['icon', 'iconPosition']
+        props: {
+            icon: {},
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator(value) {
+                    return value === 'left' || value === 'right'
+                }
+            }
+        }
     }
 </script>
 
@@ -16,6 +33,10 @@
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
 
         &:hover {
             border-color: var(--border-color-hover);
@@ -27,6 +48,27 @@
 
         &:focus {
             outline: none;
+        }
+
+        > .icon {
+            order: 1;
+            margin-right: .1em;
+        }
+
+        > .content {
+            order: 2;
+        }
+
+        &.icon-right {
+            > .content {
+                order: 1;
+            }
+
+            > .icon {
+                order: 2;
+                margin-left: .1em;
+                margin-right: 0;
+            }
         }
     }
 </style>
