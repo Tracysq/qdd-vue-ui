697b89a0042bef4c3d64c0274ebab29b9cd9724f
sunqi <sunqi@xiao100.com>
2021-03-03 15:21:04 +0800

    校验 button group 子元素 nodeName

--- src/button-group.vue
+++ src/button-group.vue
@@ -5,7 +5,16 @@
 </template>
 
 <script>
-
+    export default {
+        mounted() {
+            for (let node of this.$el.children) {
+                let name = node.nodeName.toLocaleLowerCase()
+                if (name !== 'button') {
+                    console.log(`qdd-button-group 的子元素应该全是 button，但你写的是 ${name}`)
+                }
+            }
+        }
+    }
 </script>
 
 <style lang="scss">
@@ -15,7 +24,10 @@
 
         > .qdd-button {
             border-radius: 0;
-            margin-left: -1px;
+
+            &:not(:first-child) {
+                margin-left: -1px;
+            }
 
             &:first-child {
                 border-top-left-radius: var(--border-radius);
